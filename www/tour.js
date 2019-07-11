3e2d5eb970d53264394694de1f8325749f2ce9ae
Gavin Kelly <gavin.kelly@crick.ac.uk>
2019-07-11 12:53:35 +0100

    Add automatic tab selection for js tour

--- www/tour.js
+++ www/tour.js
@@ -1,28 +1,29 @@
 var tour = new Tour({
     name: "tour",
     container: "body",
-    smartPlacement: true,
     keyboard: true,
     storage: false,
     framework: "bootstrap4",
+    backdrop: true,
     localization: {buttonTexts :{
-	nextButton:"Go on",
-	prevButton:"Go back",
-	pauseButton:"Wait a sec",
+	nextButton:"Next",
+	prevButton:"Prev",
+	pauseButton:"Pause",
 	resumeButton:"Continue",
-	endTourButton:"It's over",
+	endTourButton:"Finish Tour",
     }},
     steps: [
 	{
 	    element: "#main_tab",
 	    title: "Startup Screen",
+	    onShow: function() {$("#main_tab").tab('show');},
 	    content: "We'll walk through the main features of the the DNA damage-repair simulator here.  This is the main screen which summarises all the simulations.",
 	    placement: "right"
 	},
 	{
 	    element: "#cartoon",
 	    title: "Cartoon of example gene",
-	    content: "Here we see a single simulation of the polymerases running along a specific gene. As the gene is so long, we have to wrap it starting from top left to bottom right.  Damage sites are highlighted in red, and repaired sites in green.  As time progresses, polymerases are introduced and depending on the way the simulation has been set up, they encounter damage sites and start to queue up.  These stalled polymerases can be removed either by dissociation (in which case they are available for initiation again) or degraded - in either case, their exit is visualised by their falling to the bottom of the animation. Moving polymerases are also subject to removal from the gene body by processivity.",
+	    content: "<p>Here we see a single simulation of the polymerases running along a specific gene. As the gene is so long, we have to wrap it starting from top left to bottom right.</p><p>Damage sites are highlighted in red, and repaired sites in green.  As time progresses, polymerases are introduced and depending on the way the simulation has been set up, they encounter damage sites and start to queue up.</p><p>These stalled polymerases can be removed either by dissociation (in which case they are available for initiation again) or degraded - in either case, their exit is visualised by their falling to the bottom of the animation.</p><p>Moving polymerases are also subject to removal from the gene body by processivity.</p>",
 	    placement: "right"
 	},
 	{
@@ -52,57 +53,59 @@ var tour = new Tour({
 	{
 	    element: "#scenarios_tab",
 	    title: "Scenarios",
-	    content: "Click on this tab to allow the simulation to compare different hypothetical scenarios as to how cells behave viz a viz their polymerase dynamics",
-	    reflex: true, 
+	    content: "Use this tab to allow the simulation to compare different hypothetical scenarios as to how cells behave viz a viz their polymerase dynamics.",
+	    // reflex: true, reflexOnly: true,
+	    onNext: function() {$(this.element).tab('show');},
+	    onPrev: function() {$("#main_tab").tab('show');},
 	    placement: "right"
 	},
 	{
-	    element: "#scene_cards:first-child",
+	    element: "#scene-cards>div:first",
 	    title: "Scenarios",
-	    content: "Here is a specific scenario. You can change the label by clicking on the 'no_degrad' text; change the colour using the selector to the left; remove the scenario by clicking on the 'x' icon; add a new scenario by the button at the bottom left of the page.",
+	    delayOnElement: {delayElement: "element" },
+	    content: "<p>Here is a specific scenario. You can change the label by clicking on the text in the header; change the colour using the selector to the right; remove the scenario by clicking on the 'x' icon; add a new scenario by the button at the bottom left of the page.</p><p>Within each panel are the density of polymerases across different genes for this scenario - later on we'll see how to set up these different genes</p",
 	    placement: "right"
 	},
 	{
-	    element: "#scene_cards input:first",
+	    element: "#scene-cards form:first",
 	    title: "Scenario values",
 	    content: "These are specific values that distinguish the scenarios being simulated.  You can add and remove parameters by clicking the button at the bottom right of the page:",
 	    placement: "right"
 	},
 	{
-	    element: "#scene-footer div ",
+	    element: "#scene-footer div:first",
 	    title: "Scenario parameters",
-	    content: "Here you can select which parameters distinguish the scenarios: click one that already distinguishes scenarios removes it, so it's takes the default value across all scenarios; click a new one allows you then to tune it for each scenario indivually",
+	    content: "Here you can select which parameters distinguish the scenarios: click one that already distinguishes scenarios removes it, so it's takes the default value across all scenarios; click a new one allows you then to tune it for each scenario individually",
 	    placement: "right"
 	},
 	{
 	    element: "#genes_tab",
 	    title: "Genes",
-	    content: "Click on this tab to allow multiple genes to exist in the same cell.",
-	    reflex: true, 
+	    onNext: function() {$(this.element).tab('show');},
+	    onPrev: function() {$("#scenarios_tab").tab('show');},
+	    content: "Use this tab to allow multiple genes to exist in the same cell.",
 	    placement: "right"
 	},
 	{
-	    element: "#gene_cards:first-child",
-	    title: "Genes", content: "Here is a specific gene. These will operate in the same cell, and compete for polymerases. This is typically to allow multiple gene lengths, but heterogeneity between genes in the same cell could be modelled here.  You can weight their presence by using the 'Proportion in genome' parameter.",
+	    element: "#gene-cards>div:first",
+	    delayOnElement: {delayElement: "element" },
+	    title: "Genes", content: "<p>Here is a specific gene. These will operate in the same cell, and compete for polymerases. This is typically to allow multiple gene lengths, but heterogeneity between genes in the same cell could be modelled here.  You can weight their presence by using the 'Proportion in genome' parameter.</p><p>You can see all the scenarios illustrated here, so this is the place to look to compare scenarios directly: the colours are as determined in the previous 'scenarios' tab.</p>",
 	    placement: "right"
 	},
 	{
 	    element: "#recalculate_tab",
-	    title: "Run Simulation",
-	    content: "Click on this tab to simulate a new set of parameters, scenarios and genes",
-	    reflex: true, 
+	    title: "Recalculate results",
+	    onNext: function() {$(this.element).tab('show');},
+	    onPrev: function() {$("#genes_tab").tab('show');},
+	    content: "Use this tab to simulate a new set of parameters, scenarios and genes",
 	    placement: "right"
 	},
 	{
 	    element: "#recalculate button",
 	    title: "Run Simulation",
+	    delayOnElement: {delayElement: "element" },
+	    onNext: function() {$("#main_tab").tab('show');},
 	    content: "This button will send the parameters back to the server to run the simulation.  It may take quite a while: each scenario is simulated in turn, and a counter should progress to 100 for each. Once completed, the animations on the previous tabs will be rebuilt, and this page will refresh with the ability to download overall stats and movie files for the simulation",
-	    placement: "right"
-	},
-	{
-	    element: "#main_tab",
-	    title: "Startup Screen",
-	    content: "We'll walk through the main features of the the DNA damage-repair simulator here.  This is the main screen which summarises all the simulations.",
 	    placement: "right"
 	}
     ]
